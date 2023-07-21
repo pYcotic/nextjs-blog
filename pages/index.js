03ec279367b06e757d9020681be5132dc3da6bf6
Marco Slabbert <60457007+pYcotic@users.noreply.github.com>
2023-07-21 12:52:05 +0700

    finished product

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Bsc Computer Science Undergraduate at Goldsmiths College, University of London</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
