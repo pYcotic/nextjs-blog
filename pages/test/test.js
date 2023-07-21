ea510130e964445df23a28aa9c0f4ae98e52abe1
Marco Slabbert <60457007+pYcotic@users.noreply.github.com>
2023-07-21 14:39:31 +0700

    second test

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,9 +1,14 @@
 import Layout from "../../components/layout";
+import Head from "next/head";
 
 export default function Test() {
 	return (
 		<Layout>
+			<Head>
+				<title>Test</title>
+			</Head>
 			<h1>Test</h1>
 		</Layout>
 	);
 }
+
