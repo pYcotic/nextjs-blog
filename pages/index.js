c20c7e247a4d8cf0564f2a2f6c6d810d330b3903
Marco Slabbert <60457007+pYcotic@users.noreply.github.com>
2023-07-23 10:04:11 +0700

    Added navbar and footer

--- pages/index.js
+++ pages/index.js
@@ -32,15 +32,15 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title}) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
+        {allPostsData.slice(0, 5).map(({ id, date, title}) => (
+          <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>{title}</Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
+        ))}
         </ul>
       </section>
     </Layout>
