71a3e0b4fccd7ab95234a3601d32fd07bc17f74b
pYcotic <jslabbert.marco@gmail.com>
2024-07-21 12:01:08 +0200

    Updated flex columns

--- portfolio/app/components/examples/display/flexColumn/index.tsx
+++ portfolio/app/components/examples/display/flexColumn/index.tsx
@@ -4,10 +4,10 @@ export default function FlexColumn() {
 	return (
 		<>
 			<div className={styles.container}>
-				<div className={styles.item}></div>
-				<div className={styles.item}></div>
-				<div className={styles.item}></div>
-				<div className={styles.item}></div>
+				<div className={styles.item1}>1</div>
+				<div className={styles.item2}>2</div>
+				<div className={styles.item3}>3</div>
+				<div className={styles.item4}>Some content to justify</div>
 			</div>
 		</>
 	)
